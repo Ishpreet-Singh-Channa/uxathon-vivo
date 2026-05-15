23ea37d8b9d9e9fe77825e1c4704724753c18de9
Ishpreet Singh <ishpreet7521@gmail.com>
2026-05-15 21:56:50 +0530

    adding new allowed origin

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["http://localhost:3000"],
-  /* config options here */
+    allowedDevOrigins: ["http://localhost:3000", "http://192.168.200.108:3000"],
+    /* config options here */
 };
 
 export default nextConfig;
